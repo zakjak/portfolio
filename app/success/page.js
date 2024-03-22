aca2c4e1447af2defba069fe703d5ef483c04852
zakjak <78931393+zakjak@users.noreply.github.com>
2024-03-22 02:59:50 -0500

    added success card component

--- app/success/page.js
+++ app/success/page.js
@@ -1,30 +1,10 @@
-'use client'
-import { Card } from 'flowbite-react'
-import { useEffect } from 'react';
-import { FaCircleCheck } from "react-icons/fa6";
-import { useRouter } from 'next/navigation'
+import SuccessCard from '@/components/SuccessCard';
 
 const page = () => {
 
-  const Router = useRouter()
-
-  useEffect(() => {
-    setInterval(() => {
-      Router.push('/')
-    }, 2000)
-  })
-
   return (
     <div className='w-full min-h-screen flex items-center'>
-      <div className='w-full h-full bg-transparent flex justify-center'>
-        <Card className='shadow-xl text-center bg-zinc-200 w-[30%] h-[30rem] rounded-lg flex items-center justify-start'>
-          <div className='flex justify-center text-[4em]'>
-            <FaCircleCheck className='text-center text-green-500' />
-          </div>
-          <h5 className='text-gray-500'>Thank's For Contacting me!!!</h5>
-          <p className='text-gray-500 text-sm'>You will be redirected to the homepage!!!</p>
-        </Card>
-      </div>
+      <SuccessCard />
     </div>
   )
 }
