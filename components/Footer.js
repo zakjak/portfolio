4e1dea57dadaa613a553d99ce5dece0e064e1288
zakjak <78931393+zakjak@users.noreply.github.com>
2024-03-25 14:20:31 -0500

    changed ui of footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import { FaRegCopyright } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div className='w-full bg-black card text-white h-12 flex items-center justify-center'>
+    <div className='w-full bg-card card text-black h-12 flex items-center justify-center'>
         <div className='gap-1 bg-card w-full h-full flex items-center justify-center'>
             <FaRegCopyright />
             <span>{new Date().getFullYear()}</span>
